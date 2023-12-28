702e82b4b906230552c7d05be88523b1e41d2b3a
Pushkina Olga <archillas@mail.ru>
2023-12-29 00:31:18 +0300

    done

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,19 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
 
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+    public Bird() {
+        super();
+        color = "blue";
+        numberOfPaws = 2;
+        hasFur = false;
+    }
+
+    @Override
+    public String getDescription() {
+        return "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,18 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+        public Dog() {
+            super();
+            color = "brown";
+            numberOfPaws = 4;
+            hasFur = true;
+        }
 
+    @Override
+    public String getDescription() {
+        return "This animal is mostly brown. It has 4 paws and a fur.";
+    }
 }
